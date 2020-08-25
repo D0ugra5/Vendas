c912c6128db8c8db2aa66116aa657b9b9aeb5ebb
Jabinho <douglasmb87@gmail.com>
2020-08-25 02:20:29 -0300

    make it better

---
+++ config/db.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV == "production"){
+
+module.exports = {mongoURI:"mongodb+srv://DevsJabinho:Jabinho@13<password>@cluster0.f46a1.mongodb.net/<dbname>?retryWrites=true&w=majority"}
+
+}else{
+
+module.exports = {mongoURI :"mongodb://localhost/blogapp"}
+
+
+
+}
---
+++ models/Pagamentos.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const PagamentoStatus = new Schema({
+
+idPagamento:{
+
+    type: String,
+    required:true
+
+
+
+},
+
+
+
+
+
+    valorTotal: {
+
+        type: Number,
+        required: true
+
+
+    },
+    frete: {
+
+        type: String,
+        required: true
+
+    },
+    Endereco: {
+        type: String,
+        required: true
+    },
+    Numero: {
+        type: String,
+        required: true
+    },
+    complemento: {
+        type: String,
+       
+    },
+    observacoes: {
+        type: String,
+       
+    },
+    IdUsuario: {
+        type: String,
+        required: true
+    },
+
+    Produtos:{
+
+        type:Object,
+        required:true
+        
+    },
+ 
+
+
+
+
+
+
+})
+
+
+
+mongoose.model("pagamentos", PagamentoStatus)
