e8dcfeea48d70ae7219ec14d80ff78592088d237
Jabinho <douglasmb87@gmail.com>
2020-08-25 02:45:24 -0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production"){
 
-module.exports = {mongoURI:"mongodb+srv://DevsJabinho:Jabinho@13@cluster0.f46a1.mongodb.net/<dbname>?retryWrites=true&w=majority"}
+module.exports = {mongoURI:"mongodb+srv://DevsJabinho:Jabinho@13@cluster0.f46a1.mongodb.net/Cluster0?retryWrites=true&w=majority"}
 
 }else{
 
